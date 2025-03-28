8d4408f72b9b7ede58c881e8a1853c93bf70d14c
roman <roman@coffeebean.pro>
2025-03-28 04:35:33 -0400

    Added coffeebean env vars
    Also changed to heredoc and surrounded $BASHRC with double quotes for
    safety: if path will have spaces.

--- config/bashrc.sh
+++ config/bashrc.sh
@@ -1,11 +1,23 @@
 bashrc() {
   BASHRC="$HOME/.bashrc"
+  cp ~/.bashrc ~/.bashrc.bkp
 
   if ! grep -q "umask" $BASHRC; then
-    cp ~/.bashrc ~/.bashrc.bkp
-    echo "" >>$BASHRC
-    echo "# defaulting directories to 755 (777-22) and files to 644 (666-22)" >>$BASHRC
-    echo "umask 022" >>$BASHRC
+    cat <<EOF >>"$BASHRC"
+
+# defaulting directories to 755 (777-22) and files to 644 (666-22)
+umask 022
+EOF
+  fi
+
+  if ! grep -q "coffeebean" $BASHRC; then
+    cat <<EOF >>"$BASHRC"
+
+# coffeebean config
+export SERVER_ID="server1"
+export ENV="development"
+export PORT="8080"
+EOF
   fi
 }
 
